ea52bb53cea23940e0c65b2f62e605079eb60933
Gabriel Soares Costa <gabrielsoarescosta.gsc@gmail.com>
2021-09-13 10:27:44 -0300

    Initial structure

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
 import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="valid-keys">
+        <span className="matched">gab</span>
+        <span className="remainder">inha</span>
+      </div>
+      <div className="typed-keys">hfgabroul</div>
+      <div className="completed-words">
+        <ol>
+          <li>cidade</li>
+          <li>garrafa</li>
+          <li>tela</li>
+          <li>quadro</li>
+          <li>janela</li>
+          <li>parafuso</li>
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default App;
