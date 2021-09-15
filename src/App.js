e1a4081fd10dce5d5f55e6cda13abc2ae898e534
Gabriel Soares Costa <gabrielsoarescosta.gsc@gmail.com>
2021-09-15 11:42:08 -0300

    Added words.json and created the typed keys historic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+import wordList from "./resources/words.json";
+
+const MAX_TYPED_KEYS = 25;
+
+const getWord = () => {
+  const index = Math.floor(Math.random() * wordList.length);
+  const word = wordList(index);
+  return word.toLowerCase();
+};
 
 const App = () => {
+  const [typedKeys, setTypedKeys] = useState([]);
+  const handleKeyDown = (e) => {
+    e.preventDefault();
+    const { key } = e; // const key = e.key;
+    setTypedKeys((prev) => [...prev, key].slice(MAX_TYPED_KEYS * -1));
+  };
+
   return (
-    <div className="container">
+    <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
       <div className="valid-keys">
         <span className="matched">gab</span>
         <span className="remainder">inha</span>
       </div>
-      <div className="typed-keys">hfgabroul</div>
+      <div className="typed-keys">
+        {typedKeys ? typedKeys.join(" ") : null}&nbsp;
+      </div>
       <div className="completed-words">
         <ol>
           <li>cidade</li>
