feafa013e5bd9806d9e7fea772c6e81a0c18ef4b
Gabriel Soares Costa <gabrielsoarescosta.gsc@gmail.com>
2021-09-15 15:36:01 -0300

    Display matched and remainder letters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import wordList from "./resources/words.json";
 
@@ -6,23 +6,58 @@ const MAX_TYPED_KEYS = 25;
 
 const getWord = () => {
   const index = Math.floor(Math.random() * wordList.length);
-  const word = wordList(index);
+  const word = wordList[index];
   return word.toLowerCase();
+};
+
+const isValidKey = (key, word) => {
+  if (!word) return false;
+  const result = word.split("").includes(key);
+  return result;
+};
+
+const Word = ({ word, validKeys }) => {
+  if (!word) return null;
+  const joinedKeys = validKeys.join("");
+  const matched = word.slice(0, joinedKeys.length);
+  const remainder = word.slice(joinedKeys.length);
+
+  return (
+    <>
+      <span className="matched">{matched}</span>
+      <span className="remainder">{remainder}</span>
+    </>
+  );
 };
 
 const App = () => {
   const [typedKeys, setTypedKeys] = useState([]);
+  const [validKeys, setValidKeys] = useState([]);
+  const [word, setWord] = useState("");
+
+  useEffect(() => {
+    setWord(getWord());
+  }, []);
+
   const handleKeyDown = (e) => {
     e.preventDefault();
     const { key } = e; // const key = e.key;
     setTypedKeys((prev) => [...prev, key].slice(MAX_TYPED_KEYS * -1));
+
+    if (isValidKey(key, word)) {
+      setValidKeys((prev) => {
+        const isValidLength = prev.length <= word.length;
+        const isNextChar = isValidLength && word[prev.length] === key;
+
+        return isNextChar ? [...prev, key] : prev;
+      });
+    }
   };
 
   return (
     <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
       <div className="valid-keys">
-        <span className="matched">gab</span>
-        <span className="remainder">inha</span>
+        <Word word={word} validKeys={validKeys} />
       </div>
       <div className="typed-keys">
         {typedKeys ? typedKeys.join(" ") : null}&nbsp;
